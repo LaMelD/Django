311f0d65aadc673aa20bbfdd0b56c7e9e29cac27
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-12 04:07:55 +0900

    홀수 피라미드

--- D3/8016.cpp
+++ D3/8016.cpp
@@ -11,11 +11,11 @@ int main(int argc, char** argv)
 	{
 		unsigned long long n;
 		cin >> n;
-        //일반식이 필요하다
+        	//일반식이 필요하다
 		unsigned long long a = (n - 1) * (n - 1) * 2 + 1;
 		unsigned long long b = n * n * 2 - 1;
-        //#test_case a b
-        //형식으로 출력
+        	//#test_case a b
+        	//형식으로 출력
 		cout << "#" << test_case << " " << a << " " << b << "\n";
 	}
 	return 0;
