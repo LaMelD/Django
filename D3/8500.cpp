d8aa09d762fd8974470d33fda7b85435b8a18de9
LaMelD <54210310+LaMelD@users.noreply.github.com>
2019-09-12 04:02:09 +0900

    극장 좌석

---
+++ D3/8500.cpp
@@ -0,0 +1,45 @@
+//극장 좌석
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+int solution(vector<int> v)
+{
+	sort(v.begin(), v.end());
+	int ret = v.front() + v.back() + v.size();
+	for (int i = 0; i < v.size() - 1; i++)
+	{
+		ret += max(v[i], v[i + 1]);
+	}
+
+	return ret;
+}
+
+int main(int argc, char** argv)
+{
+	int test_case;
+	int T;
+	cin >> T;
+	for (test_case = 1; test_case <= T; ++test_case)
+	{
+		int n;
+		cin >> n;
+		vector<int> nv;
+		for (int i = 0; i < n; i++)
+		{
+			int ni;
+			cin >> ni;
+			nv.push_back(ni);
+		}
+		cout << '#' << test_case << " ";
+		cout << solution(nv) << endl;
+	}
+	return 0;
+}
